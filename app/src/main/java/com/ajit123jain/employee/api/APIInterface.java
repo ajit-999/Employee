5160ea91412d7b0e2ca55746845fd81d361abe19
ajit-999 <ajitkumarjain123@gmail.com>
2017-11-04 17:58:52 +0530

    added search bar

---
+++ app/src/main/java/com/ajit123jain/employee/api/APIInterface.java
@@ -0,0 +1,29 @@
+package com.ajit123jain.employee.api;
+
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by ajit on 4/11/17.
+ */
+
+interface APIInterface {
+
+   /* @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job); */
+
+}
